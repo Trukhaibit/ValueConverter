e9e78bfbc725156998b44b8d73d04dab8bbfed77
Trukhaibit <70858117+Trukhaibit@users.noreply.github.com>
2022-03-15 12:20:01 -0400

    Add files via upload

---
+++ ValueConverter/ValueConverter/MilestoKilosConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ValueConverter
+{
+    class MilestoKilosConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double miles;
+            if (double.TryParse((string)value, out miles))
+            {
+                double kilos = miles * 1.60934;
+
+                return kilos.ToString("f2");
+            }
+            else
+            {
+                return value;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
